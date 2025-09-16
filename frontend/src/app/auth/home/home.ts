2d5fc2633200b97e390e3fe8e99b262b991f63a8
jonimende <jonasyjere15@gmail.com>
2025-09-16 16:11:37 -0300

    add frontend for create-event and home

--- frontend/src/app/auth/home/home.ts
+++ frontend/src/app/auth/home/home.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface Event {
+  id: number;
+  title: string;
+  description?: string;
+  starts_at: string;
+  capacity: number;
+  price: number;
+  image_path?: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrls: ['./home.scss']
 })
-export class Home {
+export class Home implements OnInit {
+  events: Event[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+  const token = localStorage.getItem('token');
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`
+  });
+
+    this.http.get<{ data: Event[] }>('http://localhost:8000/api/events', { headers }).subscribe({
+      next: (res) => {
+    this.events = res.data; 
+    },
+    error: (err) => {
+      console.error('Error al obtener eventos:', err);
+    }
+    });
+  }
 }
